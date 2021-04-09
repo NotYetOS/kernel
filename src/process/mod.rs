e800a3da711dda622633bc99596257aa00888f10
Spxg <itsme@unsafe.me>
2021-04-09 20:47:01 +0800

    target: back to riscv64gc

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -5,13 +5,6 @@ mod manager;
 mod pid;
 
 pub fn test() {
-    use crate::task::TaskUnit;
-    use crate::fs::ROOT;
-    use unit::ProcessUnit;
-    use fefs::inode::INodeType;
-    use alloc::vec::Vec;
-    use alloc::sync::Arc;
-    
     println!("");
     println!("[test] process");
     println!("----------------------->");
