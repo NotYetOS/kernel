ce1172d209a7bb4272c50e3a318a2f8110028182
Spxg <itsme@unsafe.me>
2021-04-20 23:42:01 +0800

    stdio: update read() to match sbi which return -1 for failure

--- src/fs/stdio.rs
+++ src/fs/stdio.rs
@@ -1,10 +1,51 @@
 use crate::alloc::string::ToString;
 use crate::sbi::console_getchar;
 use alloc::string::String;
+use lazy_static::lazy_static;
+use spin::Mutex;
+use alloc::vec::Vec;
 use super::{
     File, 
     UserBuffer
 };
+
+lazy_static! {
+    static ref BYTE_BUFFER: Mutex<Vec<u8>> = Mutex::new(Vec::new());
+}
+
+lazy_static! {
+    static ref CHARS: Mutex<String> = Mutex::new(String::new());
+}
+
+fn push_byte(byte: u8) {
+    BYTE_BUFFER.lock().push(byte);
+}
+
+fn generate_chars() -> bool {
+    let mut buf_lock = BYTE_BUFFER.lock();
+    let mut chars_lock = CHARS.lock();
+    let len = buf_lock.len();
+    match core::str::from_utf8(&buf_lock[0..len]) {
+        Ok(chars) => {
+            chars_lock.push_str(chars);
+            buf_lock.clear();
+            true
+        }
+        Err(_) => {
+            buf_lock.clear();
+            false
+        }
+    }
+}
+
+fn chars_len() -> usize {
+    CHARS.lock().len()
+}
+
+fn getchar_from_chars() -> char {
+    let ch = CHARS.lock().remove(0);
+    ch
+}
 
 pub struct Stdin;
 
@@ -14,24 +55,49 @@ impl File for Stdin {
 
     fn read(&self, mut buf: UserBuffer) -> usize {
         let mut ptr = buf.inner[0].as_mut_ptr();
-        let value = console_getchar();
-        if value == -1 { return value as usize; }
 
-        let ch = char::from_u32(value as u32).unwrap();
-        if ch.len_utf8() > buf.len() { 
-            let ret = -2; 
-            return ret as usize; 
-        }
+        let mut read_func = || {
+            let ch = getchar_from_chars();
 
-        let chars = ch.to_string();
-        for byte in chars.bytes() {
-            unsafe {
-                ptr.write_volatile(byte);
-                ptr = ptr.add(1);
+            let chars = ch.to_string();
+            for byte in chars.bytes() {
+                unsafe {
+                    ptr.write_volatile(byte);
+                    ptr = ptr.add(1);
+                }
             }
+            
+            ch.len_utf8()
+        };
+
+        if chars_len() != 0 {
+            return read_func();
         }
-        
-        ch.len_utf8()
+
+        let first_byte = loop {
+            let value = console_getchar();
+            if value != -1 {
+                break value as u8;
+            }
+        };
+
+        push_byte(first_byte);
+
+        loop {
+            let value = console_getchar();
+            if value != -1 {
+                push_byte(value as u8);
+            } else {
+                if !generate_chars() {
+                    let ret = -1;
+                    return ret as usize;
+                } else {
+                    break;
+                }
+            }
+        };
+
+        read_func()
     }
 
     fn write(&self, _: UserBuffer) -> usize {
